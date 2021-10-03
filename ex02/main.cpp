f7d2c7038704b26d405004c4615db59023909fa5
Ysoroko <yarossoroko@gmail.com>
2021-10-03 21:08:58 +0200

    main adapted with sections + attack after death for FragTrap added

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 17:30:34 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/03 21:03:09 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/03 21:08:26 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,41 @@ int	main(void)
 	std::string s2 = "Scarlet";
 	std::string s3 = "Fred";
 
+	// Constructors
 	ClapTrap claptrap(s1);
 	ScavTrap scavtrap(s2);
 	FragTrap fragtrap(s3);
-
 	std::cout << std::endl;
+
+	// Attacks for each class
 	fragtrap.attack(s2);
 	claptrap.attack(s2);
 	scavtrap.attack(s1);
 	std::cout << std::endl;
-	fragtrap.attack(s2);
-	fragtrap.attack(s2);
-	fragtrap.attack(s2);
-	fragtrap.attack(s2);
-	
-	std::cout << std::endl;
-	fragtrap.highFivesGuys();
 
+	// New class attack until exhaustion
+	fragtrap.attack(s2);
+	fragtrap.attack(s2);
+	fragtrap.attack(s2);
+	fragtrap.attack(s2);
 	std::cout << std::endl;
+
+	// New class personal method call
+	fragtrap.highFivesGuys();
+	std::cout << std::endl;
+
+	// Repair over max hp
 	fragtrap.beRepaired(18);
+
+	// Take damage
 	fragtrap.takeDamage(50);
+
+	// Die
 	fragtrap.takeDamage(50);
+
+	// Do something after dying
 	fragtrap.beRepaired(20);
+	fragtrap.attack(s2);
 
 	std::cout << std::endl;
 	return (0);
